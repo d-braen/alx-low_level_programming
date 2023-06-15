442bcf298b1deefd4309275f2af71ead08d9df6b
benop7 <benjaminpetegnong@gmail.com>
2023-06-15 13:06:48 +0100

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - my program
+ *
+ * Return: 0 on success otherwise, number > 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
