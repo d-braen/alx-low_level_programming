e8fb95bdf038a6bbd1beac3a5aec7d3089cd7c0d
benop7 <benjaminpetegnong@gmail.com>
2023-06-15 13:54:50 +0100

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program
+ *
+ * Return: 0 on success 1 to indicate error occured
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of a int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long: %ld byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+
+	return (0);
+}
