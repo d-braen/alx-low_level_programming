79ff75a1e80da6b17ee10a230c89c64a395d5070
d-braen <danielsbrand77@gmail.com>
2023-06-20 11:51:27 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value computation
+ *
+ * @n: integer
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
