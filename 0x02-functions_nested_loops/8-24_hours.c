1d1dc8beb05987a5202e8665783a6f8b73435c14
d-braen <danielsbrand77@gmail.com>
2023-06-20 13:15:32 -0700

    task 8t

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print each minute in the day of the life of jack bauer
+ *
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			if (h / 10)
+			{
+				_putchar('0' + h / 10);
+				_putchar('0' + h % 10);
+			}
+			else
+			{
+				_putchar('0');
+				_putchar('0' + h);
+			}
+			_putchar(':');
+			if (m / 10)
+                        {
+                                _putchar('0' + m / 10);
+                                _putchar('0' + m % 10);
+                        }
+                        else
+                        {
+                                _putchar('0');
+                                _putchar('0' + m);
+                        }
+			_putchar('\n');
+		}
+	}
+}
