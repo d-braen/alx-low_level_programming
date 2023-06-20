0497ac5a40689874a734d20fd752134abfb2793a
d-braen <danielsbrand77@gmail.com>
2023-06-20 11:17:03 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lower case
+ *
+ * Return: 0 on complete otherwise 1
+ */
+void print_alphabet(void)
+{
+	int idx;
+	int a;
+
+	for (idx = 0; idx < 10; idx++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+			_putchar('\n');
+		}
+	}
+}
