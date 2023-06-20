20150b5ef794d66c0133ffe78328b80d94d5c694
d-braen <danielsbrand77@gmail.com>
2023-06-20 10:54:28 -0700

    Init subdirectory and do task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/**
+ * main - print putchar on stdout
+ *
+ * Return: 0 on successful execution otherwise non-zero
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
