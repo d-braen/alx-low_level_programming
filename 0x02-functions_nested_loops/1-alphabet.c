61a8911b9d954ee35bae742295d2f9df4330eb2a
d-braen <danielsbrand77@gmail.com>
2023-06-20 11:04:57 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lower case
+ *
+ * Return: 0 on complete otherwise 1
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
