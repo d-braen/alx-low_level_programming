41e8d3e6ae77d6591efaa77381a134e124da5ba8
d-braen <danielsbrand77@gmail.com>
2023-06-20 08:46:48 -0700

    task100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two digits
+ *
+ * Return: 0 on complete otherwise 1
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+
+			putchar(a);
+			putchar(b);
+			if (a != '8' || b != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
