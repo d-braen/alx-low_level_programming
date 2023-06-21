451a8045626eb3473243c1dc0f0837212462685e
d-braen <danielsbrand77@gmail.com>
2023-06-21 09:04:54 -0700

    task 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/** main - print first 50 fibonacci numbers
+ *
+ * Return: 0 on success, non-zero otherwise
+ */
+int main(void)
+{
+	long int current = 1;
+	long int next = 2;
+	int i = 2;
+	long int tmp = 0;
+
+	putchar('1');
+
+	while (i <= 50)
+	{
+		printf(", %ld", next);
+		tmp = next;
+		next += current;
+		current = tmp;	
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
