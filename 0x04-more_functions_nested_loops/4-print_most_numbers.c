b896f611fc5a84a67c44d13206d72793eb5e04d0
benop7 <benjaminpetegnong@gmail.com>
2023-06-22 20:41:48 +0100

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 through 9
+ *
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+			_putchar('0' + i);
+		i++;
+	}
+	_putchar('\n');
+}
