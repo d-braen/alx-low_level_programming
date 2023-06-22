05a2ba4cab7d7152dc094b01ad3fc80c6d3679eb
benop7 <benjaminpetegnong@gmail.com>
2023-06-22 22:08:18 +0100

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - fizz buzz program
+ *
+ *Return: 0 on success
+ */
+int main(void)
+{
+	int n = 1;
+
+	printf("%d", n);
+
+	while (++n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
+	}
+	printf("\n");
+}
