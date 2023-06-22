dffa6cc17649acc7a4dd9ece0322e5884d116f61
benop7 <benjaminpetegnong@gmail.com>
2023-06-22 21:31:15 +0100

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal using backslash
+ * @n: length of the diagonal
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+	int space = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		while (j < space)
+		{
+			_putchar(' ');
+			j++;
+		}
+		++space;
+		_putchar('\');
+		_putchar('\n');
+			
+}
