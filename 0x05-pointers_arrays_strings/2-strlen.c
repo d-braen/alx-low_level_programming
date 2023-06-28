550d532aec0e479e156b52747a0fe52393ee4a4f
benop7 <benjaminpetegnong@gmail.com>
2023-06-28 11:56:29 +0100

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,16 @@
 /**
  * _strlen - returns the length of a string
  * @s: character pointer (string)
- *
+ * Return: length of the string
  */
 int _strlen(char *s)
 {
 	int len = 0;
 
+	if (*s == '\0')
+	{
+		return (len);
+	}
 	while (s[++len])
 		;
 
